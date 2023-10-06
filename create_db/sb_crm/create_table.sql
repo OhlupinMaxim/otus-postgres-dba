6096633f3548ec01e76ba9ef4a6f420ec7e3c30e
Maxim Ohlupin <maximohlupin.omd.work@gmail.com>
2023-10-06 13:42:35 +0300

    update create

--- create_db/sb_crm/create_table.sql
+++ create_db/sb_crm/create_table.sql
@@ -2,7 +2,7 @@
 set search_path = sb_crm_schema;
 
 CREATE TABLE sb_crm_schema.Staff (
-	id INTEGER PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	username varchar(64) NOT NULL CHECK (username != ''),
 	password varchar(64) NOT NULL CHECK (password != ''),
 	staff_full_name varchar(64) NOT NULL CHECK (staff_full_name != ''),
@@ -10,42 +10,36 @@ CREATE TABLE sb_crm_schema.Staff (
 );
 
 CREATE TABLE sb_crm_schema.Product (
-	id INTEGER PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	name varchar(64) NOT NULL CHECK (name != ''),
 	article INTEGER NOT NULL UNIQUE CHECK (article > 0),
 	manufacturer varchar(256)
 );
 
 CREATE TABLE sb_crm_schema.Product_Unit (
-	id INTEGER PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	fk_product INTEGER NOT NULL,
 	unit varchar(8) NOT NULL CHECK (unit != ''),
 	price_per_unit REAL NOT NULL CHECK (price_per_unit > 0)
 );
 
 CREATE TABLE sb_crm_schema.Shop_Point (
-	id INTEGER PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	name varchar(64) NOT NULL UNIQUE CHECK (name != ''),
 	address varchar(64) NOT NULL UNIQUE CHECK (address != '')
 );
 
-CREATE TABLE sb_crm_schema.Warehouse (
-	id INTEGER PRIMARY KEY,
-	oto_shop_point INTEGER UNIQUE NOT NULL,
-	FOREIGN KEY (oto_shop_point) REFERENCES sb_crm_schema.Shop_Point(id)
-);
-
-
-CREATE TABLE sb_crm_schema.Product_Warehouse(
-	id INTEGER PRIMARY KEY,
+CREATE TABLE sb_crm_schema.Shop_Point_Product(
+	id SERIAL PRIMARY KEY,
 	fk_product INTEGER NOT NULL,
-	fk_warehouse INTEGER NOT NULL,
+	fk_shop_point INTEGER NOT NULL,
 	FOREIGN KEY (fk_product) REFERENCES sb_crm_schema.Product(id),
-	FOREIGN KEY (fk_warehouse) REFERENCES sb_crm_schema.Warehouse(id)
+	FOREIGN KEY (fk_shop_point) REFERENCES sb_crm_schema.Shop_Point(id),
+	count_products REAL
 );
 
 CREATE TABLE sb_crm_schema.Work_Shift (
-	id INTEGER PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	fk_staff INTEGER NOT NULL,
 	fk_shop_point INTEGER NOT NULL,
 	date_shift DATE DEFAULT now(),
@@ -57,13 +51,12 @@ CREATE TABLE sb_crm_schema.Work_Shift (
 );
 
 CREATE TABLE sb_crm_schema.Amount_Product (
-	id INTEGER PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	fk_work_shift INTEGER NOT NULL,
 	fk_product INTEGER NOT NULL,
 	fk_product_unit INTEGER NOT NULL,
 	number_of_sold INTEGER CHECK (number_of_sold > 0),
 	FOREIGN KEY (fk_work_shift) REFERENCES sb_crm_schema.Work_Shift(id),
-	FOREIGN KEY (fk_product) REFERENCES sb_crm_schema.Product(id),
 	FOREIGN KEY (fk_product_unit) REFERENCES sb_crm_schema.Product_Unit(id)
 );
 
